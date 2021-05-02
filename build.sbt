defab569ca906dafe96b4a4e14b95a081637e154
Aaditya <aaditya@sparker0i.me>
2021-05-02 21:53:45 +0530

    Variables and Datatypes

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Scala-Beginners"
+
+version := "0.1"
+
+scalaVersion := "2.13.5"
---
+++ src/main/scala/basics/VariablesDatatypes.scala
@@ -0,0 +1,35 @@
+package basics
+
+object VariablesDatatypes extends App {
+  //Variables: Container which holds value
+  val a: Int = 5
+  val b: Char = 'a'
+  val c: String = "ASDSADASDASDASdsdsdasdasdas"
+
+  val maxByte: Byte = 127
+  val minByte: Byte = -128
+
+  val maxShort: Short = 32767
+  val minShort: Short = -32768
+
+  val maxInt: Int = 2147483647
+  val minInt: Int = -2147483648
+
+  val maxLong: Long = 9223372036854775807L
+  val minLong: Long = -9223372036854775808L
+
+  val someFloat: Float = 2.3F
+  val someDouble: Double = 3.14
+
+  val someBoolean: Boolean = true
+  val falseBoolean: Boolean = false
+
+  val someChar: Char = 'a'
+  val someString: String = "ABCDEFG"
+
+  var x: Int = 5
+  x = 6
+  x = x + 3
+
+  val y = 27
+}
